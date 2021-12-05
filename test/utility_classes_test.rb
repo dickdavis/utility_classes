292cfdbfb8b24824eb07eb37532b613c22531a1f
Dick Davis <dick@hey.com>
2021-12-05 16:29:28 -0600

    Wrote basic test suite

--- test/utility_classes_test.rb
+++ test/utility_classes_test.rb
@@ -8,4 +8,28 @@ class UtilityClassesTest < ActiveSupport::TestCase
   test 'it has a version number' do
     assert UtilityClasses::VERSION
   end
+
+  test 'it loads the config file' do
+    assert UtilityClasses.for(:heading)
+  end
+
+  test 'it returns base styles for a given type key' do
+    assert_equal 'font-semibold text-center', UtilityClasses.for(:heading)
+  end
+
+  test 'it returns base and variant styles for a given type/variant key combination' do
+    assert_equal 'font-semibold text-center text-4xl', UtilityClasses.for(:heading, variant: :grande)
+  end
+
+  test 'it raises an exception when the provided type key is not found' do
+    assert_raises UtilityClasses::Exceptions::TypeKeyNotFound do
+      UtilityClasses.for(:foo)
+    end
+  end
+
+  test 'it raises an exception when the provided variant key is not found' do
+    assert_raises UtilityClasses::Exceptions::VariantKeyNotFound do
+      UtilityClasses.for(:heading, variant: :foo)
+    end
+  end
 end
